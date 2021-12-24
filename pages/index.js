e2f66bbb41040dd0008593ea4b4b4673ed093b37
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-24 21:09:02 +0800

    fix title

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-3xl font-bold">
-        Unofficial <a className="text-blue-600" href="https://github.com/mkrl/misbrands">Misbrands</a> UI
+        Unofficial <a className="text-blue-600" href="https://github.com/mkrl/misbrands">Misbrands</a> Showcase
         </h1>
         {names.map(name => (
           <img key={name} width="500" height="500" src={`https://raw.githubusercontent.com/mkrl/misbrands/456ea8aa2c7873123dce3d746528bb9a3b6dd139/${name}.svg`} alt="a" />
