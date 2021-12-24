0f29f220ffbb70d4b3937bb9f2ae85750fe9f055
Yik San Chan <evan.chanyiksan@gmail.com>
2021-12-24 21:13:26 +0800

    add star

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,12 @@ export default function Home() {
       <Head>
         <title>Misbrands</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-3xl font-bold">
-        Unofficial <a className="text-blue-600" href="https://github.com/mkrl/misbrands">Misbrands</a> Showcase
+        Unofficial <a className="text-blue-600" href="https://github.com/mkrl/misbrands">Misbrands</a> Showcase <a class="github-button" href="https://github.com/yiksanchan/misbrands" data-icon="octicon-star" aria-label="Star yiksanchan/misbrands on GitHub">Star</a>
         </h1>
         {names.map(name => (
           <img key={name} width="500" height="500" src={`https://raw.githubusercontent.com/mkrl/misbrands/456ea8aa2c7873123dce3d746528bb9a3b6dd139/${name}.svg`} alt="a" />
